9b97ff990fe9c6b849bec6e47585999f7eef347f
ldrohan <drohanluke@gmail.com>
2015-08-14 13:57:07 -0700

    added name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814201835) do
+ActiveRecord::Schema.define(version: 20150814202959) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150814201835) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
